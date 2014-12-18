de3f0ad939f102d4974a6e63542021131c081ccc
kedzie <mark.kedzierski@gmail.com>
2014-12-18 15:20:23 -0600

    used compiler plugin

--- compiler/src/main/java/com/github/kedzie/drools/Compiler.java
+++ compiler/src/main/java/com/github/kedzie/drools/Compiler.java
@@ -3,12 +3,17 @@ package com.github.kedzie.drools;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
 import org.drools.core.common.DroolsObjectOutputStream;
+import org.drools.core.util.DroolsStreamUtils;
 import org.kie.api.io.ResourceType;
 import org.kie.internal.builder.KnowledgeBuilder;
 import org.kie.internal.builder.KnowledgeBuilderConfiguration;
+import org.kie.internal.builder.KnowledgeBuilderError;
 import org.kie.internal.builder.KnowledgeBuilderFactory;
 import org.kie.internal.definition.KnowledgePackage;
 import org.kie.internal.io.ResourceFactory;
@@ -20,28 +25,36 @@ import java.util.List;
 import java.util.Properties;
 
 /**
- * Created by kedzie on 6/5/14.
+ * @author kedzie
+ *
  */
-@Mojo(name = "compile-rules")
+@Mojo(name = "compile-rules",
+      requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME,
+      requiresProject = true,
+      defaultPhase = LifecyclePhase.COMPILE,
+      configurator = "include-project-dependencies")
 public class Compiler extends AbstractMojo {
 
     /**
      * DRL rule package
      */
-    @Parameter( property = "compile-rules.ruleFiles" )
+    @Parameter(property = "compile-rules.ruleFiles",required = true)
     private List<String> ruleFiles;
 
     /**
      * Output folder
      */
-    @Parameter( property = "compile-rules.outputDirectory", defaultValue = "${project.basedir}/res/raw" )
+    @Parameter(property = "compile-rules.outputDirectory", defaultValue = "${project.basedir}/res/raw" )
     private String outputDirectory;
 
     /**
      * KnowledgeBuilderConfiguration properties
      */
-    @Parameter( property = "compile-rules.properties" )
+    @Parameter(property = "compile-rules.properties" )
     private Properties properties;
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -55,6 +68,10 @@ public class Compiler extends AbstractMojo {
                 kbuilder.add(ResourceFactory.newClassPathResource(ruleFile), ResourceType.DRL);
             }
             if (kbuilder.hasErrors()) {
+                getLog().error("=====Errors=====");
+                for(KnowledgeBuilderError error : kbuilder.getErrors()) {
+                    getLog().error(error.toString());
+                }
                 throw new MojoFailureException(kbuilder.getErrors().toString());
             }
             final Collection<KnowledgePackage> pkgs = kbuilder.getKnowledgePackages();
@@ -62,12 +79,12 @@ public class Compiler extends AbstractMojo {
             outputFolder.mkdirs();
             for(KnowledgePackage pkg : pkgs) {
                 //serialize knowledge package
+                getLog().info("Writing package: " + pkg.getName());
                 File drlFile = new File(outputFolder, pkg.getName().replace('.', '_'));
                 drlFile.getParentFile().mkdirs();
                 FileOutputStream out = new FileOutputStream(drlFile);
-                DroolsObjectOutputStream oos = new DroolsObjectOutputStream(out);
-                oos.writeObject(pkg);
-                oos.close();
+                DroolsStreamUtils.streamOut(out, pkg);
+                out.close();
             }
         } catch (Exception e) {
             throw new MojoExecutionException("error", e);
---
+++ compiler/src/main/java/com/github/kedzie/drools/IncludeProjectDependenciesComponentConfigurator.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2014 CNH Industrial NV. All rights reserved.
+ *
+ * This software contains proprietary information of CNH Industrial NV. Neither
+ * receipt nor possession thereof confers any right to reproduce, use, or
+ * disclose in whole or in part any such information without written
+ * authorization from CNH Industrial NV.
+ */
+
+package com.github.kedzie.drools;
+
+import org.codehaus.classworlds.ClassRealm;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.configurator.AbstractComponentConfigurator;
+import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
+import org.codehaus.plexus.component.configurator.ComponentConfigurator;
+import org.codehaus.plexus.component.configurator.ConfigurationListener;
+import org.codehaus.plexus.component.configurator.converters.composite.ObjectWithFieldsConverter;
+import org.codehaus.plexus.component.configurator.converters.special.ClassRealmConverter;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluator;
+import org.codehaus.plexus.configuration.PlexusConfiguration;
+import org.codehaus.plexus.logging.Logger;
+import org.codehaus.plexus.logging.console.ConsoleLogger;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+      * A custom ComponentConfigurator which adds the project's runtime classpath elements
+      * to the
+      *
+      * @author Brian Jackson
+      * @since Aug 1, 2008 3:04:17 PM
+      *
+      * @plexus.requirement role="org.codehaus.plexus.component.configurator.converters.lookup.ConverterLookup"
+      *                   role-hint="default"
+      */
+@Component(role=ComponentConfigurator.class, hint="include-project-dependencies")
+public class IncludeProjectDependenciesComponentConfigurator extends AbstractComponentConfigurator {
+
+   private static final Logger LOGGER = new ConsoleLogger(Logger.LEVEL_DEBUG, "Configurator");
+
+   public void configureComponent( Object component, PlexusConfiguration configuration,
+                                   ExpressionEvaluator expressionEvaluator, ClassRealm containerRealm,
+                                   ConfigurationListener listener )
+         throws ComponentConfigurationException {
+
+      addProjectDependenciesToClassRealm(expressionEvaluator, containerRealm);
+
+      converterLookup.registerConverter( new ClassRealmConverter( containerRealm ) );
+
+      ObjectWithFieldsConverter converter = new ObjectWithFieldsConverter();
+
+      converter.processConfiguration( converterLookup, component, containerRealm.getClassLoader(), configuration,
+            expressionEvaluator, listener );
+   }
+
+   private void addProjectDependenciesToClassRealm(ExpressionEvaluator expressionEvaluator, ClassRealm containerRealm) throws ComponentConfigurationException {
+      List<String> runtimeClasspathElements;
+      try {
+         //noinspection unchecked
+         runtimeClasspathElements = (List<String>) expressionEvaluator.evaluate("${project.runtimeClasspathElements}");
+      } catch (ExpressionEvaluationException e) {
+         throw new ComponentConfigurationException("There was a problem evaluating: ${project.runtimeClasspathElements}", e);
+      }
+
+      // Add the project dependencies to the ClassRealm
+      final URL[] urls = buildURLs(runtimeClasspathElements);
+      for (URL url : urls) {
+         containerRealm.addConstituent(url);
+      }
+   }
+
+   private URL[] buildURLs(List<String> runtimeClasspathElements) throws ComponentConfigurationException {
+      // Add the projects classes and dependencies
+      List<URL> urls = new ArrayList<URL>(runtimeClasspathElements.size());
+      for (String element : runtimeClasspathElements) {
+         try {
+            final URL url = new File(element).toURI().toURL();
+            urls.add(url);
+            if (LOGGER.isDebugEnabled()) {
+               LOGGER.debug("Added to project class loader: " + url);
+            }
+         } catch (MalformedURLException e) {
+            throw new ComponentConfigurationException("Unable to access project dependency: " + element, e);
+         }
+      }
+
+      // Add the plugin's dependencies (so Trove stuff works if Trove isn't on
+      return urls.toArray(new URL[urls.size()]);
+   }
+
+}
